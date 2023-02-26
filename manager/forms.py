758e970dfbc1b5775fca17fb0e662b481a06a623
Anna Kabatova <kabatova.anna@ukr.net>
2023-02-26 17:10:45 +0200

    Add forms

---
+++ manager/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+from manager.models import Student, Task
+
+
+class TaskForm(forms.ModelForm):
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Task
+        fields = "__all__"
+
+
+class TaskSearchForm(forms.Form):
+    model = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by task name"}),
+    )
+
+
+class StudentCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Student
+        fields = UserCreationForm.Meta.fields + (
+            "group",
+            "first_name",
+            "last_name",
+        )
+
+
+class StudentGroupUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ["group"]
+
+
+class StudentSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by username"}),
+    )
