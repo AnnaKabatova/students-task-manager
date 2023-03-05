0d53dacb90b4820e4e37720a6bbda2ceb5683f63
Anna Kabatova <kabatova.anna@ukr.net>
2023-03-05 16:51:23 +0200

    Added tests for search forms

--- manager/tests/test_models.py
+++ manager/tests/test_models.py
@@ -4,18 +4,18 @@ import pytz
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-from manager.models import Task, TaskType, Group, Student
+from manager.models import Task, TaskType, Group
 
 
 class ModelsTests(TestCase):
     def test_task_type_str(self):
         task_type = TaskType.objects.create(name="test")
         self.assertEqual(str(task_type), f"{task_type.name}")
-
+    
     def test_group_str(self):
         group = Group.objects.create(name="test")
         self.assertEqual(str(group), f"{group.name}")
-
+    
     def test_task_str(self):
         europe_kiev = pytz.timezone('Europe/Kiev')
         task = Task.objects.create(
@@ -25,7 +25,7 @@ class ModelsTests(TestCase):
             deadline=datetime(2024, 3, 5, 14, 30, tzinfo=europe_kiev),
         )
         self.assertEqual(str(task), f"{task.name}")
-
+    
     def test_student_str(self):
         student = get_user_model().objects.create_user(
             username="test",
---
+++ manager/tests/test_search_forms.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from manager.models import Student, Task
+
+
+class TestSearch(TestCase):
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(
+            username="test_user"
+        )
+        self.client.force_login(self.user)
+
+    def test_search_students_by_username(self):
+        response = self.client.get(
+            reverse("manager:student-list") + "?name=test_user"
+        )
+        self.assertEqual(
+            list(response.context["student_list"]),
+            list(Student.objects.filter(username__icontains="test_user")),
+        )
+
+    def test_search_task_by_name(self):
+        response = self.client.get(
+            reverse("manager:task-list") + "?name=write"
+        )
+        self.assertEqual(
+            list(response.context["task_list"]),
+            list(Task.objects.filter(name__icontains="write")),
+        )
