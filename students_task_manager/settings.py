0b13c12a59c1c03a4897b35289679b423db6e467
Anna Kabatova <kabatova.anna@ukr.net>
2023-03-07 13:45:49 +0200

    Updated Allowed Hosts again

--- students_task_manager/settings.py
+++ students_task_manager/settings.py
@@ -20,7 +20,7 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
-    "https://students-task-manager-wgn1.onrender.com"
+    "students-task-manager-wgn1.onrender.com"
 ]
 
 INTERNAL_IPS = [
